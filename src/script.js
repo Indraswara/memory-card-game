0cdbdfdb9488315e7fff39b9c5f6a131441a60e7
InTheRain <indrswaragalihjayanegara@gmail.com>
2024-06-26 11:15:37 +0700

    script

--- src/script.js
+++ src/script.js
@@ -1,13 +1,51 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.memory-card');
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
 
     function flipCard() {
-        console.log("Flipped");
-        this.classList.toggle('flip');
+        if (lockBoard) return; 
+        if (this === firstCard) return; 
+
+        this.classList.add('flip');
+
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+        } else {
+            hasFlippedCard = false;
+            secondCard = this;
+
+            checkForMatch();
+        }
+    }
+
+    function checkForMatch() {
+        let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+        if (isMatch) {
+            disableCards();
+        } else {
+            lockBoard = true; 
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                resetBoard();
+            }, 1500); 
+        }
+    }
+
+    function disableCards() {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+        resetBoard();
+    }
+
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
     }
 
     cards.forEach(card => card.addEventListener('click', flipCard));
 });
-
